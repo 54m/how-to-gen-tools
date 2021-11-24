b63e8e5cd4d72d3b9bac822770a97393c6f5f6fb
54m <30588003+54m@users.noreply.github.com>
2021-11-24 19:00:45 +0900

    feat: add user model

---
+++ back/domain/model/user.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"context"
+
+	"github.com/54m/how-to-gen-tools/back/pkg/validator"
+	"golang.org/x/xerrors"
+)
+
+// User - ユーザー
+type User struct {
+	ID   string `json:"id"   firestore:"-" firestore_key:"auto"` // ID
+	Name string `json:"name"`                                    // ニックネーム
+	Age  int    `json:"age"`                                     // 年齢
+	Meta
+}
+
+// Prepare - 新規/更新とValidation
+func (obj User) Prepare(context.Context) (*User, error) {
+	if err := validator.Validate(obj); err != nil {
+		return nil, xerrors.Errorf("validation error: %w", err)
+	}
+
+	return &obj, nil
+}
